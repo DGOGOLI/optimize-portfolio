a619fca7426b892966b13d9cd26669c26b1dd9bf
Denis Gogoli <gogolidenis@gmail.com>
2025-09-26 22:04:04 +0200

    function to get data from yahoofinance

---
+++ data/get_data.py
@@ -0,0 +1,30 @@
+import yfinance as yf
+import pandas as pd
+
+def get_data(tickers, start_date, end_date=None, save=False):
+
+    assert len(tickers) > 1, "We need at least two tickers to evaluate the efficient strategy"
+
+    print(f"Collecting data for tickers {tickers}...")
+    df = yf.download(tickers, start=start_date, end=end_date, progress=True, auto_adjust=False)["Adj Close"]
+
+    file_name = "../files/"
+
+    for i in range(0, len(tickers)):
+        t = tickers[i]
+        file_name += t
+
+        if i == len(tickers) - 1:
+            file_name += ".csv"
+            break
+
+
+        file_name += "_"
+        
+
+    if save:
+        df.to_csv(file_name, index=False)
+
+    print("Data collected")
+
+    return df
